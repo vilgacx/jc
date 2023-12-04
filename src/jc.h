e9afe799a94f690b3f77f1217ce5c9905d80569c
xorvet <cypherx702@gmail.com>
2023-12-05 02:04:34 +0530

    fns

--- src/jc.h
+++ src/jc.h
@@ -6,7 +6,7 @@ typedef unsigned int uint;
 
 typedef struct json {} JSON;
 
-typedef struct object { char* key;  char* val; } OBJECT;
+//typedef struct object { char* key;  char* val; } OBJECT;
 
 JSON parse(const char*);
 
